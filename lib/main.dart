07df884744f0613edd4fc0202082accba2ab99ef
Kay Elia <katherine.elia001@umb.edu>
2021-12-07 10:30:38 -0500

    Search page, search functionality and drawer

--- lib/main.dart
+++ lib/main.dart
@@ -70,7 +70,7 @@ class _MyAppState extends State<MyApp> {
                   nextScreen: const HomeScreen(),
                 );
               },
-              child: const Text('Onboard an @sign'),
+              child: const Text('Onboard the @sign'),
             ),
           ),
         ),
@@ -78,7 +78,46 @@ class _MyAppState extends State<MyApp> {
     );
   }
 }
-
+// The class for the drawer
+class AppDrawer extends StatelessWidget {
+  const AppDrawer({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context){
+    return Drawer(
+      child: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            const DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.purple,
+              ),
+              child: Text('Drawer Header'),
+            ),
+            ListTile(
+              title: const Text('Search DIY Journals'),
+              onTap: () {
+                   Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => const SearchPage()),
+                   );
+                //Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              title: const Text('Dashboard'),
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const HomeScreen()),
+                );
+              }
+            ),
+          ]
+      )
+    );
+  }
+}
 //* The next screen after onboarding (second screen)
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -92,18 +131,183 @@ class HomeScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Dashboard'),
       ),
-      body: Center(
-        child: Column(
-          children: [
-            const Text(
-                'Successfully onboarded and navigated to FirstAppScreen'),
+      body: Column(
+        children: [
+          const Text('Success onboarded and navigated to FirstAppScreen'),
 
-            /// Use the AtClientManager instance to get the current atsign
-            Text(
-                'Current @sign: ${atClientManager.atClient.getCurrentAtSign()}'),
-          ],
-        ),
+          /// Use the AtClientManager instance to get the current atsign
+          Text('Current @sign: ${atClientManager.atClient.getCurrentAtSign()}'),
+          ElevatedButton(
+              child: const Text('Search DIY Journals'),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const SearchPage()),
+                );
+              })
+        ],
       ),
+      drawer: const AppDrawer(),
     );
   }
 }
+
+class SearchPage extends StatefulWidget {
+  const SearchPage({Key? key}) : super(key: key);
+
+  @override
+  SearchPageState createState() => SearchPageState();
+}
+
+Icon customIcon = const Icon(Icons.search);
+Widget customSearchBar = const Text('Search DIY Journals');
+
+class SearchPageState extends State<SearchPage> {
+    TextEditingController editingController = TextEditingController();
+  //final List<String> entries = <String>['A', 'B', 'C', 'D', 
+  //'E','F','G', 'H', 'I', 'J', 'K', 'L','M','N','O','P',
+  //'Q','R', 'S', 'T', 'U','V','W','X','Y','Z'];
+final duplicateItems = List<String>.generate(100, (i) => "Item $i");
+var items = <String>[];
+
+@override
+void initState(){
+  items.addAll(duplicateItems);
+  super.initState();
+}
+
+  void filterSearchResults(String query) {
+    List<String> dummySearchList = <String>[];
+    dummySearchList.addAll(duplicateItems);
+    if(query.isNotEmpty) {
+      List<String> dummyListData = <String>[];
+      dummySearchList.forEach((item) {
+        if(item.contains(query)) {
+          dummyListData.add(item);
+        }
+      });
+      setState(() {
+        items.clear();
+        items.addAll(dummyListData);
+      });
+      return;
+    } else {
+      setState(() {
+        items.clear();
+        items.addAll(duplicateItems);
+      });
+    }
+
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Search Page'),
+        ),
+      // appBar: AppBar(
+      //   //drawer: const AppDrawer(),
+      //   title: customSearchBar,
+      //   centerTitle: true,
+      //   automaticallyImplyLeading: false,
+      //   actions: [
+      //     IconButton(
+      //         onPressed: () {
+      //           setState(() {
+      //             if (customIcon.icon == Icons.search) {
+      //               customIcon = const Icon(Icons.cancel);
+      //               customSearchBar = const ListTile(
+      //                 leading: Icon(
+      //                   Icons.search,
+      //                   color: Colors.white,
+      //                   size: 28,
+      //                 ),
+      //                 title: TextField(
+      //                   decoration: InputDecoration(
+      //                     border: InputBorder.none,
+      //                     labelText: 'type in DYI journal name...',
+      //                     labelStyle: TextStyle(
+      //                       color: Colors.white,
+      //                       fontSize: 18,
+      //                       fontStyle: FontStyle.italic,
+      //                     ),
+      //                   ),
+      //                 ),
+      //               );
+      //             } else {
+      //               customIcon = const Icon(Icons.search);
+      //               customSearchBar = const Text('Search DIY journals');
+      //             }
+      //           });
+      //         },
+      //         icon: customIcon)
+      //   ],
+      // ),
+
+ body: Column(
+   children: <Widget>[
+     Padding(
+       padding: const EdgeInsets.all(8.0),
+       child: TextField(
+         onChanged: (value) {
+           filterSearchResults(value);
+         },
+         controller: editingController,
+         decoration: const InputDecoration(
+             labelText: "Search for DIY Articles",
+             hintText: "Search",
+             prefixIcon: Icon(Icons.search),
+             border: OutlineInputBorder(
+                 borderRadius: BorderRadius.all(Radius.circular(25.0)))),
+         ),
+     ),
+     Expanded(
+       child:ListView.builder(
+         shrinkWrap: true,
+         padding: const EdgeInsets.all(8),
+         itemCount: items.length,
+         itemBuilder: (BuildContext context, int index){
+           return Container(
+             
+             height: 50,
+             color: Colors.blue[100],
+             child: Center(child: Text('Entry ${items[index]}')),
+       );
+         },
+       ),
+     ),
+   ],
+ ),
+       drawer: const AppDrawer(),
+        );
+  }
+}
+
+
+
+// class ArticleListView extends StatefulWidget {
+//   const ArticleListView({Key? key}) : super(key: key);
+
+//   @override
+//   _ArticleListViewState createState() => _ArticleListViewState();
+// }
+// class _ArticleListViewState extends State<ArticleListView> {
+
+// @override
+//   Widget build(BuildContext context) {
+// final List<String> entries = <String>['A', 'B', 'C'];
+// final List<int> colorCodes = <int>[600, 500, 100];
+//   return ListView.builder(
+//       padding: const EdgeInsets.all(8),
+//       itemCount: entries.length,
+//       itemBuilder: (BuildContext context, int index){
+//         return Container(
+//           height: 50,
+//           color: Colors.amber[colorCodes[index]],
+//           child: Center(child: Text('Entry ${entries[index]}')),
+//         );
+//       },
+//     );
+// }
+// }
