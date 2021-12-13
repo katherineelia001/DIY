77da2026ef329a342fe0fdf1a62bed92fa60320f
Kay Elia <katherine.elia001@umb.edu>
2021-12-13 14:10:15 -0500

    working to get search working on future build list

--- lib/screens/search_page.dart
+++ lib/screens/search_page.dart
@@ -1,8 +1,17 @@
-import 'dart:async';
 
 import 'package:diy/widgets/drawer.dart';
 import 'package:flutter/material.dart';
-import '../main.dart';
+import 'dart:convert';
+
+import 'package:diy/widgets/drawer.dart';
+import 'package:flutter/material.dart';
+import 'package:diy/models/article.dart';
+import 'package:diy/screens/view_article.dart';
+import 'package:diy/screens/add_article.dart';
+
+import 'package:at_client_mobile/at_client_mobile.dart';
+import 'package:at_commons/at_commons.dart';
+
 
 class SearchPage extends StatefulWidget {
   const SearchPage({Key? key}) : super(key: key);
@@ -11,86 +20,163 @@ class SearchPage extends StatefulWidget {
   SearchPageState createState() => SearchPageState();
 }
 
-Icon customIcon = const Icon(Icons.search);
-Widget customSearchBar = const Text('Search DIY Journals');
 
 class SearchPageState extends State<SearchPage> {
-  TextEditingController editingController = TextEditingController();
-  //final List<String> entries = <String>['A', 'B', 'C', 'D',
-  //'E','F','G', 'H', 'I', 'J', 'K', 'L','M','N','O','P',
-  //'Q','R', 'S', 'T', 'U','V','W','X','Y','Z'];
-  final duplicateItems = List<String>.generate(100, (i) => "Item $i");
-  var items = <String>[];
+  String? atSign = AtClientManager.getInstance().atClient.getCurrentAtSign();
+  var atClientManager = AtClientManager.getInstance();
 
-  @override
-  void initState() {
-    items.addAll(duplicateItems);
-    super.initState();
-  }
+    TextEditingController editingController = TextEditingController();
+    late List <Map <String, dynamic>> foundArticles;
 
-  void filterSearchResults(String query) {
-    List<String> dummySearchList = <String>[];
-    dummySearchList.addAll(duplicateItems);
-    if (query.isNotEmpty) {
-      List<String> dummyListData = <String>[];
-      dummySearchList.forEach((item) {
-        if (item.contains(query)) {
-          dummyListData.add(item);
-        }
-      });
-      setState(() {
-        items.clear();
-        items.addAll(dummyListData);
-      });
-      return;
-    } else {
-      setState(() {
-        items.clear();
-        items.addAll(duplicateItems);
-      });
-    }
-  }
+  // List<Map<String, dynamic>>? get yourlist => null;
+  //   @override
+  //   void initState(){
+  //     super.initState();
+  //     foundArticles = yourlist;
+  //   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Search Page'),
-      ),
-      body: Column(
-        children: <Widget>[
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: TextField(
-              onChanged: (value) {
-                filterSearchResults(value);
-              },
-              controller: editingController,
-              decoration: const InputDecoration(
-                  labelText: "Search for DIY Articles",
-                  hintText: "Search",
-                  prefixIcon: Icon(Icons.search),
-                  border: OutlineInputBorder(
-                      borderRadius: BorderRadius.all(Radius.circular(25.0)))),
-            ),
-          ),
-          Expanded(
-            child: ListView.builder(
-              shrinkWrap: true,
-              padding: const EdgeInsets.all(8),
-              itemCount: items.length,
-              itemBuilder: (BuildContext context, int index) {
-                return Container(
-                  height: 50,
-                  color: Colors.blue[100],
-                  child: Center(child: Text('Entry ${items[index]}')),
-                );
-              },
-            ),
-          ),
-        ],
-      ),
       drawer: const AppDrawer(),
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        title: const Text('Search Page'),
+        
+      ),
+      body:  Column(
+        children: [
+          FutureBuilder(
+            future: scanYourArticles(),
+            builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
+              if(snapshot.hasData) {
+                List<Map<String, dynamic>> yourlist = snapshot.data;
+    void initState(){
+      super.initState();
+      foundArticles = yourlist;
+    }
+ 
+  // This function is called whenever the text field changes
+  void _runFilter(String enteredKeyword) {
+    List<Map<String, dynamic>> results;
+    if (enteredKeyword.isEmpty) {
+      // if the search field is empty or only contains white-space, we'll display all users
+      results = yourlist;
+    } else {
+      results = yourlist
+          .where((user) =>
+              user["name"].toLowerCase().contains(enteredKeyword.toLowerCase()))
+          .toList();
+      // we use the toLowerCase() method to make it case-insensitive
+    }
+
+    // Refresh the UI
+    setState(() {
+      foundArticles = results;
+    });
+  }
+
+     Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: TextField(
+          onChanged: (value) => _runFilter(value),
+         controller: editingController,
+         decoration: const InputDecoration(
+             labelText: "Search for DIY Articles",
+             hintText: "Search",
+             prefixIcon: Icon(Icons.search),
+             border: OutlineInputBorder(
+                 borderRadius: BorderRadius.all(Radius.circular(25.0)))),
+         ),
+     );
+              Expanded(
+                  //child:foundArticles.isNotEmpty
+                  //? ListView.builder(
+                    child: ListView.builder(
+                    itemCount: foundArticles.length,
+                    itemBuilder: (BuildContext context, int index) {
+                      var articlejson =
+                          json.decode(foundArticles[index].values.elementAt(0));
+                      var article = Article.fromJson(articlejson);
+
+                      return Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: GestureDetector(
+                          onTap: () => {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => ViewArticle(
+                                  article: article,
+                                ),
+                              ),
+                            ),
+                          },
+                          child: Card(
+                            elevation: 0,
+                            child: Text(foundArticles[index].keys.elementAt(0)),
+                          ),
+                        ),
+                      );
+                    },
+                  )
+                  
+              );
+              }
+              return const Text ("HAS NO DATA");
+            },
+            
+          ),
+           ],
+      ),
     );
+  }
+
+  Future<List<Map<String, dynamic>>> scanYourArticles() async {
+    var atClientManager = AtClientManager.getInstance();
+
+    List<AtKey> response =
+        await atClientManager.atClient.getAtKeys(sharedWith: atSign);
+
+    List<Map<String, dynamic>> values = [];
+
+    for (AtKey key in response) {
+      String? keyStr = key.key;
+      if (keyStr != "signing_privatekey") {
+        var val = await lookup(key);
+        if (val != null) values.add({keyStr!: val});
+        // var isDeleted = await atClientManager.atClient.delete(key);
+        // isDeleted ? print("Deleted") : print("Not Deleted");
+      }
+    }
+
+    return values;
+  }
+    Future<List<Map<String, dynamic>>> scanNamespaceArticles() async {
+    var atClientManager = AtClientManager.getInstance();
+    String myRegex = '^(?!public).*diy.*';
+    List<AtKey> response =
+        await atClientManager.atClient.getAtKeys(regex: myRegex);
+
+    List<Map<String, dynamic>> values = [];
+
+    for (AtKey key in response) {
+      String? keyStr = key.key;
+      String val = await lookup(key);
+      values.add({keyStr!: val});
+      // var isDeleted = await atClientManager.atClient.delete(key);
+      // isDeleted ? print("Deleted") : print("Not Deleted");
+    }
+
+    return values;
+  }
+
+  Future<dynamic> lookup(AtKey? atKey) async {
+    AtClient client = atClientManager.atClient;
+    if (atKey != null) {
+      AtValue result = await client.get(atKey);
+      return result.value;
+    }
+    return null;
   }
 }
--- lib/widgets/drawer.dart
+++ lib/widgets/drawer.dart
@@ -1,15 +1,15 @@
-import 'package:diy/screens/home_screen.dart';
+
+
 import 'package:diy/screens/search_page.dart';
 import 'package:flutter/material.dart';
-// import 'package:at_client_mobile/at_client_mobile.dart';
-// import 'package:at_onboarding_flutter/at_onboarding_flutter.dart'
-//     show Onboarding;
-// import 'package:at_utils/at_logger.dart' show AtSignLogger;
-// import 'package:path_provider/path_provider.dart'
-//     show getApplicationSupportDirectory;
-// import 'package:at_app_flutter/at_app_flutter.dart' show AtEnv;
+import 'package:diy/screens/home_screen.dart';
+import 'package:diy/screens/add_article.dart';
 
-// import '../main.dart';
+
+
+
+
+import '../main.dart';
 
 // The class for the drawer. We need to add drawer: AppDrawer(),
 //to the scaffold of every page in the app so the user is always
@@ -17,48 +17,67 @@ import 'package:flutter/material.dart';
 class AppDrawer extends StatelessWidget {
   const AppDrawer({Key? key}) : super(key: key);
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context){
     return Drawer(
-        child: ListView(padding: EdgeInsets.zero, children: [
-      _createHeader(),
-      // const DrawerHeader(
-      //   decoration: BoxDecoration(
-      //     color: Colors.purple,
-      //   ),
-      //   child: Text('Drawer Header'),
-      // ),
-      ListTile(
-        leading: const Icon(Icons.search),
-        title: const Text('Search DIY Journals'),
-        onTap: () {
-          Navigator.push(
-            context,
-            MaterialPageRoute(builder: (context) => const SearchPage()),
-          );
-        },
-      ),
-      ListTile(
-          leading: const Icon(Icons.home),
-          title: const Text('Homepage'),
-          onTap: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => const HomeScreen()),
-            );
-          }),
-    ]));
+      child: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            _createHeader(),
+            ListTile(
+              leading: const Icon(Icons.search),
+              title: const Text('Search DIY Journals'),
+              onTap: () {
+                   Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => const SearchPage()),
+                   );
+              },
+            ),
+            ListTile(
+              leading: const Icon(Icons.home),
+              title: const Text('Homepage'),
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const HomeScreen()),
+                );
+              }
+            ),
+            ListTile(
+              leading: const Icon(Icons.note_add_rounded),
+              title: const Text('Add a new article'),
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const AddArticle()),
+                );
+              }
+            ),
+            ListTile(
+              leading: const Icon(Icons.article_sharp),
+              title: const Text('My articles'),
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const HomeScreen()),
+                );
+              }
+            ),
+          ]
+      )
+    );
   }
 }
 
 Widget _createHeader() {
   return DrawerHeader(
-    margin: EdgeInsets.zero,
-    padding: EdgeInsets.zero,
-    decoration: BoxDecoration(
-      color: Colors.purple[800],
-    ),
-    child: Stack(
-      children: <Widget>[
+      margin: EdgeInsets.zero,
+      padding: EdgeInsets.zero,
+      decoration: const BoxDecoration(
+          color: Colors.black,
+              ),
+      child: Stack(children: const <Widget>[
         Positioned(
             bottom: 12.0,
             left: 16.0,
@@ -67,7 +86,5 @@ Widget _createHeader() {
                     color: Colors.white,
                     fontSize: 20.0,
                     fontWeight: FontWeight.w500))),
-      ],
-    ),
-  );
+      ]));
 }
